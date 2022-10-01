7a908eeaada7101da700fb54f1fc64e49b5c8f02
zugamifk <zugamifk@gmail.com>
2022-10-01 11:18:03 -0300

    clean up skeleton

--- Assets/Scripts/View/Animators/GunTurretAnimator.cs
+++ Assets/Scripts/View/Animators/GunTurretAnimator.cs
@@ -7,6 +7,6 @@ public class GunTurretAnimator : ScriptedAnimation
 {
     protected override void UpdateAnimation(Transform bone, float x)
     {
-        bone.transform.localPosition = new Vector3(0, 0, x);
+        bone.transform.localPosition = new Vector3(0, 0, -x);
     }
 }
