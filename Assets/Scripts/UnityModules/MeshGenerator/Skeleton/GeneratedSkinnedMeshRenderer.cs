bbff17c88570e22d14efe5ce14170c6251d2a36b
zugamifk <zugamifk@gmail.com>
2022-10-01 01:00:25 -0300

    set up bones and bindposes properly

--- Assets/Scripts/UnityModules/MeshGenerator/Skeleton/GeneratedSkinnedMeshRenderer.cs
+++ Assets/Scripts/UnityModules/MeshGenerator/Skeleton/GeneratedSkinnedMeshRenderer.cs
@@ -1,24 +1,53 @@
 using MeshGenerator.Skeleton;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace MeshGenerator
 {
+    [RequireComponent(typeof(SkinnedMeshRenderer))]
     [RequireComponent(typeof(MeshSkeleton))]
     public class GeneratedSkinnedMeshRenderer : MonoBehaviour
     {
         MeshSkeleton _skeleton_cached;
         MeshSkeleton _skeleton => _skeleton_cached ??= GetComponent<MeshSkeleton>();
-        
+
+        SkinnedMeshRenderer _meshRenderer_cached;
+        SkinnedMeshRenderer _meshRenderer => _meshRenderer_cached ??= GetComponent<SkinnedMeshRenderer>();
+
+        Transform[] GetBones()
+        {
+            var bones = _skeleton.Bones;
+            var boneTfs = new Transform[bones.Count];
+            for (int i = 0; i < bones.Count; i++)
+            {
+                boneTfs[i] = bones[i].transform;
+            }
+            return boneTfs;
+        }
+
         public void ApplyMesh(Mesh mesh)
         {
-            _skeleton.ApplyMesh(mesh);
+            _meshRenderer.bones = GetBones();
+            SetBindPoses(mesh);
+            _meshRenderer.sharedMesh = mesh;
+        }
+
+        void SetBindPoses(Mesh mesh)
+        {
+            var bindPoses = new Matrix4x4[_skeleton.Bones.Count];
+            for (int i = 0;i < _skeleton.Bones.Count; i++)
+            {
+                bindPoses[i] = _skeleton.Bones[i].GetBindPose();
+            }
+            mesh.bindposes = bindPoses;
         }
 
         public void Clear()
         {
-            _skeleton.Clear();
+            _meshRenderer.sharedMesh = null;
         }
     }
 }
--- Assets/Scripts/UnityModules/MeshGenerator/Skeleton/MeshBone.cs
+++ Assets/Scripts/UnityModules/MeshGenerator/Skeleton/MeshBone.cs
@@ -6,6 +6,18 @@ namespace MeshGenerator
 {
     public class MeshBone : MonoBehaviour
     {
-        
+        [SerializeField]
+        bool _isRoot;
+
+        public Matrix4x4 GetBindPose()
+        {
+            if(_isRoot)
+            {
+                return Matrix4x4.identity;
+            } else
+            {
+                return transform.worldToLocalMatrix * transform.parent.localToWorldMatrix;
+            }
+        }
     }
 }
